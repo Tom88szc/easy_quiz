8e9d8350f153ca0de77173de609dc8e3da98782f
tom88szc <osobinskitomasz@gmail.com>
2020-06-02 21:42:19 +0200

    PROJ - Add Models

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    createDate = models.DateTimeField('date created')
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
+    answered_count = models.IntegerField(default=0)
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
