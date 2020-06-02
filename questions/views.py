72ff1dae771f2cbef4e95f2fe9980695a0092a37
tom88szc <osobinskitomasz@gmail.com>
2020-06-02 20:55:49 +0200

    PROJ - add questions views

--- questions/views.py
+++ questions/views.py
@@ -5,4 +5,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Witaj w aplikacji Easy Quiz!")
+    question = {
+        'text': 'Jak bardzo wysoki jest Mount Everest',
+        'answers': [
+            {'text': '3323 m', 'correct': False},
+            {'text': '8999 m', 'correct': False},
+            {'text': '8848 m', 'correct': True},
+            {'text': '3323 m', 'correct': False},
+        ]
+    }
+    ctx = {
+        'question': question,
+    }
+    return render(request, 'questions/question.html', ctx)
